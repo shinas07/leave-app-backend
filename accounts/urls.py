e7c074ad2ea4e0f16e201699a94856ad7bbc17df
shinas07 <shinasamanpk@gmail.com>
2025-01-11 19:11:30 +0530

    feat: add manger registration endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LoginView, LogoutView, RefreshTokenView, UserDetailView
+from .views import LoginView, LogoutView, RefreshTokenView, UserDetailView,RegisterManagerView
 
 
 urlpatterns = [
+    path('register-manager/', RegisterManagerView.as_view(), name='register-manager'),  
     path('login/',LoginView.as_view(), name='login'),
     path('logout/',LogoutView.as_view(),name='logout'),
     path('refresh/',RefreshTokenView.as_view(), name='refresh'),

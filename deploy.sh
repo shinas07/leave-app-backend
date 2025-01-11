fb325a34093716017367a471794740c66e8baaf2
shinas07 <shinasamanpk@gmail.com>
2025-01-11 17:31:09 +0530

    fix: Add condition to check for existing superuser before creating a new one in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-ECHO is on.
 #!/bin/bash
 # deploy.sh
 
@@ -13,7 +12,13 @@ python manage.py collectstatic --no-input
 echo "Running migrations..."
 python manage.py migrate
 
+# Check if superuser exists before creating
+echo "Checking if superuser exists..."
+if python manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(username='$SUPERUSER_USERNAME').exists())" | grep -q "True"; then
+    echo "Superuser already exists. Skipping superuser creation."
+else
+    echo "Creating superuser..."
+    python manage.py createsuperuser --noinput --username $SUPERUSER_USERNAME --email $SUPERUSER_EMAIL --user_type admin
+fi
 
-# # Optionally create a superuser (uncomment if needed)
-# echo "Creating superuser..."
-#    python manage.py createsuperuser --noinput --username $SUPERUSER_USERNAME --email $SUPERUSER_EMAIL  --user_type admin
+

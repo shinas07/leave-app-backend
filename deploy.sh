39f2fbbeede5859e7e0e90d27e36cb270138a036
shinas07 <shinasamanpk@gmail.com>
2025-01-11 19:13:25 +0530

    fix: removed admin and manager creation throough shell

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+ECHO is on.
 #!/bin/bash
 # deploy.sh
 
@@ -12,22 +13,7 @@ python manage.py collectstatic --no-input
 echo "Running migrations..."
 python manage.py migrate
 
-# Check if superuser exists before creating
-echo "Checking if superuser exists..."
-if python manage.py shell -c "from accounts.models import User; print(User.objects.filter(username='$SUPERUSER_USERNAME').exists())" | grep -q "True"; then
-    echo "Superuser already exists. Skipping superuser creation."
-else
-    echo "Creating superuser..."
-    python manage.py createsuperuser --noinput --username $SUPERUSER_USERNAME --email $SUPERUSER_EMAIL --user_type admin
-fi
 
-
-# Check if manager exists before creating
-echo "Checking if manager exists..."
-if python manage.py shell -c "from accounts.models import User; print(User.objects.filter(email='manager@gmail.com').exists())" | grep -q "True"; then
-    echo "Manager already exists. Skipping manager creation."
-else
-    echo "Creating manager..."
-    python manage.py shell -c "from accounts.models import User; User.objects.create_user(username='manager', email='manager@gmail.com', password='Manager@1234', user_type='manager')"
-fi
-
+# Optionally create a superuser (uncomment if needed)
+# echo "Creating superuser..."
+#    python manage.py createsuperuser --noinput --username $SUPERUSER_USERNAME --email $SUPERUSER_EMAIL  --user_type admin

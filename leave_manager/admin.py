69008a8ea326bd6e2cda87a0c6845192eeb6a5da
shinas07 <shinasamanpk@gmail.com>
2025-01-20 20:16:09 +0530

    feat:register `YearlyLeaveBalance` model in admin panel

--- leave_manager/admin.py
+++ leave_manager/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import LeaveRequest
+from .models import LeaveRequest,YearlyLeaveBalance
 # Register your models here.
 
 admin.site.register(LeaveRequest)
+admin.site.register(YearlyLeaveBalance)

3da83f290592d95bd7a2d2d0cb8a66289022979f
shinas07 <shinasamanpk@gmail.com>
2025-01-21 19:06:11 +0530

    feat: add duration field and logic for calculating working days in leave request

--- leave_manager/models.py
+++ leave_manager/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from accounts.models import User
 from django.core.exceptions import ValidationError
+from datetime import timedelta
 
 class LeaveRequest(models.Model):
     LEAVE_TYPE_CHOICES = [
@@ -12,6 +13,7 @@ class LeaveRequest(models.Model):
     leave_type = models.CharField(max_length=20, choices=LEAVE_TYPE_CHOICES)
     start_date = models.DateField()
     end_date = models.DateField()
+    duration = models.IntegerField(default=0) 
     reason = models.TextField()
     status = models.CharField(max_length=20, choices=[
         ('pending', 'Pending'),
@@ -20,9 +22,41 @@ class LeaveRequest(models.Model):
     ], default='pending')
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def calculate_duration(self):
+            if not self.start_date or not self.end_date:
+                return 0
+                
+            working_days = 0
+            current_date = self.start_date
+            
+            while current_date <= self.end_date:
+                if current_date.weekday() != 6:  # Skip Sundays
+                    working_days += 1
+                current_date += timedelta(days=1)
+                
+            return working_days
+
     def clean(self):
         if self.start_date > self.end_date:
             raise ValidationError('End date must be after start date.')
 
+    def save(self, *args, **kwargs):
+        self.duration = self.calculate_duration()
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return f"{self.user.username} - {self.get_leave_type_display()} ({self.status})"
+    
+
+class YearlyLeaveBalance(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    year = models.IntegerField()
+    total_leaves = models.IntegerField(default=40)
+    leaves_taken = models.IntegerField(default=0)
+
+    class Meta:
+        unique_together = ('user', 'year')
+
+    @property
+    def remaining_leaves(self):
+        return self.total_leaves - self.leaves_taken
